47811062b8e359fb7bf6bba70cc419b5d4a8f371
Linzn <niklas.linz@mirranet.de>
2026-07-19 23:01:29 +0200

    upload

---
+++ src/main/java/de/linzn/growatt/GrowattPlugin.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2026 MirraNET, Niklas Linz. All rights reserved.
+ *
+ * This file is part of the MirraNET project and is licensed under the
+ * GNU Lesser General Public License v3.0 (LGPLv3).
+ *
+ * You may use, distribute and modify this code under the terms
+ * of the LGPLv3 license. You should have received a copy of the
+ * license along with this file. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>
+ * or contact: niklas.linz@mirranet.de
+ */
+
+package de.linzn.growatt;
+
+
+
+import de.linzn.stem.modules.pluginModule.STEMPlugin;
+
+
+public class GrowattPlugin extends STEMPlugin {
+
+    public static GrowattPlugin growattPlugin;
+
+
+    public GrowattPlugin() {
+        growattPlugin = this;
+    }
+
+    @Override
+    public void onEnable() {
+        this.getDefaultConfig().save();
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+}
